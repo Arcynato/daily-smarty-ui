660b71264f603d51e1cc69b29cba32a0ba234d9c
Arcynato <james.hostetter64@gmail.com>
2020-05-25 13:34:47 -0400

    Continuing code

---
+++ src/components/home.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import Logo from './logo';
+import SearchBar from './searchbar';
+import RecentPosts from './recentposts';
+
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Home extends Component {
+  handleSearchBarSubmit(query) {
+    this.props.fetchPostsWithQuery(query, () => {
+      this.props.history.push('/results');
+    });
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <Logo />
+        <SearchBar
+          page="home"
+          onSubmit={(query) => this.handleSearchBarSubmit(query)}
+        />
+        <RecentPosts />
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(Home);
--- src/components/logo.js
+++ src/components/logo.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
+import { Link } from 'react-router-dom';
 
 export default class Logo extends Component {
   render() {
+    const size = {
+      height: this.props.size ? this.props.size : 105,
+      width: this.props.size ? this.props.size : 105,
+    };
+
     return (
       <div className="logo-main">
-        <img
-          alt="daily smarty ui image logo big"
-          src="/assets/ds_circle_logo.png"
-        />
+        <Link to="/">
+          <img
+            style={size}
+            alt="daily smarty ui image logo big"
+            src="/assets/ds_circle_logo.png"
+          />
+        </Link>
       </div>
     );
   }
